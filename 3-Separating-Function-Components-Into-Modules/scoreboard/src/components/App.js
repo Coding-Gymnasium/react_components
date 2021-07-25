aff2565cbe74bb33a83a5aab45aa99ee83c9c81f
Nico Rithner <nicorithner@gmail.com>
2021-07-25 11:20:55 -0600

    Complete lesson 'Controlled Components'

--- 3-Separating-Function-Components-Into-Modules/scoreboard/src/components/App.js
+++ 3-Separating-Function-Components-Into-Modules/scoreboard/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Player from './Player';
+import AddPlayerForm from './AddPlayerForm';
 
 class App extends Component {
   state = {
@@ -71,6 +72,7 @@ class App extends Component {
             removePlayer={this.handleRemovePlayer}           
           />
         )}
+        < AddPlayerForm />
       </div>
     );
   }
